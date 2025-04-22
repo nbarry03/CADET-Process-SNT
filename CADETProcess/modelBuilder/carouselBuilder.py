c5a8f644b2b01a0b53210df97af4188a6530d91e
Nick Barry-Hallee <nick.barryhallee@summitnanotech.com>
2025-04-22 14:18:17 -0600

    Update column setter to handle new Column class

--- CADETProcess/modelBuilder/carouselBuilder.py
+++ CADETProcess/modelBuilder/carouselBuilder.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from functools import wraps
-from typing import Any, Optional, NoReturn, List
+from typing import Any, Optional, NoReturn, List, Tuple, Iterable
 import warnings
 
 import numpy as np
@@ -183,7 +183,8 @@ class CarouselBuilder(Structure):
         self.component_system = component_system
         self.name = name
         self._flow_sheet = FlowSheet(component_system, name)
-        self._column:List[Column] = []
+        self._column = None
+        self._columns: List[Column] = []
 
     @property
     def flow_sheet(self) -> FlowSheet:
@@ -191,18 +192,18 @@ class CarouselBuilder(Structure):
         return self._flow_sheet
 
     @property
-    def column(self) -> List[TubularReactorBase]:
-        """List[TubularReactorBase]: The column template for all zones."""
+    def column(self) -> Tuple[TubularReactorBase, ...]:
+        """Tuple[TubularReactorBase]: The column template for all zones."""
         return self._column
 
     @column.setter
-    def column(self, column: TubularReactorBase | List[TubularReactorBase]) -> NoReturn:
+    def column(self, column: TubularReactorBase | Iterable[TubularReactorBase]) -> NoReturn:
         if isinstance(column, TubularReactorBase):
-            column = [column]
-        elif isinstance(column, (list, tuple)) and all(
-            isinstance(c, TubularReactorBase) for c in column
+            column = (column,)
+        elif (isinstance(column, Iterable)
+              and all(isinstance(c, TubularReactorBase) for c in column)
         ):
-            column = list(column)
+            column = tuple(column)
         else:
             raise TypeError("Column must be an instance or list of TubularReactorBase.")
         for col in column:
@@ -210,6 +211,7 @@ class CarouselBuilder(Structure):
                 raise CADETProcessError("Number of components does not match.")
 
         self._column = column
+        self._columns.clear()
 
     @wraps(FlowSheet.add_unit)
     def add_unit(self, *args, **kwargs):
