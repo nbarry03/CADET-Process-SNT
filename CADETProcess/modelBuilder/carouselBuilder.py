b2afdb7e71b8545e71f9ac6b0907e128f59bf3b2
Nick Barry-Hallee <nick.barryhallee@summitnanotech.com>
2025-05-01 09:08:31 -0600

    Fixed private _columns references, switched to access via property.

--- CADETProcess/modelBuilder/carouselBuilder.py
+++ CADETProcess/modelBuilder/carouselBuilder.py
@@ -315,7 +315,7 @@ class CarouselBuilder(Structure):
                     subunits.append(sub)
 
                 # Aggregate Column objects
-                self._columns.append(
+                self.columns.append(
                     Column(index = col_index, subunits = subunits)
                 )
                 col_index += 1
@@ -352,18 +352,18 @@ class CarouselBuilder(Structure):
     def _add_intra_zone_connections(self, flow_sheet: FlowSheet) -> NoReturn:
         """Add connections within column template and within zones."""
         # Connect subunits within each column
-        for col in self._columns:
+        for col in self.columns:
             for upstream, downstream in zip(col.subunits, col.subunits[1:]):
                 flow_sheet.add_connection(upstream, downstream)
 
         # Connect zone inlets/outlets to column tops/bottoms respectively
         for zone in self.zones:
-            for col in self._columns:
+            for col in self.columns:
                     flow_sheet.add_connection(zone.inlet_unit, col.top)
                     flow_sheet.add_connection(col.bottom, zone.outlet_unit)
         
         # Connect each bottom of each column to the top of next
-        cols = self._columns
+        cols = self.columns
         for this_col, next_col in zip(cols, cols[1:] + cols[:1]):
             flow_sheet.add_connection(this_col.bottom, next_col.top)
 
@@ -418,7 +418,7 @@ class CarouselBuilder(Structure):
                     col_indices,
                     carousel_state
                 )
-                cols = [self._columns[i] for i in rotated_indices]
+                cols = [self.columns[i] for i in rotated_indices]
 
                 if isinstance(zone, SerialZone):
                     evt = process.add_event(
@@ -447,7 +447,7 @@ class CarouselBuilder(Structure):
 
                 elif isinstance(zone, ParallelZone):
                     # Create split vector with n_columns number of slots
-                    total = len(self._columns)
+                    total = len(self.columns)
                     split = [0.0] * total
                     share = 1.0 / zone.n_columns
 
